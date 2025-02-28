bf962ce5da32b0e51475b29cc0d053bc7bca2f69
younes_qasempour <qasempouryounes@gmail.com>
2025-02-28 12:45:00 +0330

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from data_manager import DataManager
+from flight_search import FlightSearch
+from flight_data import FlightData
+from notification_manager import NotificationManager
+import requests
+from dotenv import dotenv_values
+import os
+
+secrets = dotenv_values(".env")
+
